f26414f9be5352219576273645547c4f181cfb15
krishnajakodali <krishnajakodali@gmail.com>
2020-06-14 22:14:59 +0530

    linalg

--- geometry/linalg/codes/conic2/conic2d.py
+++ geometry/linalg/codes/conic2/conic2d.py
@@ -15,13 +15,13 @@ for i in range(len(x)):
     y=p(x)
 
 #zeroes
-s=math.sqrt(b**2-4*a*c)
-k1=(-1*b+s)/(2*a)
-k2=(-1*b-s)/(2*a)
+#s=math.sqrt(b**2-4*a*c)
+#k1=(-1*b+s)/(2*a)
+#k2=(-1*b-s)/(2*a)
+roots=p.r
 
-
-A=np.array([k1,p(k1)])
-B=np.array([k2,p(k2)])
+A=np.array([roots[0],0])
+B=np.array([roots[1],0])
 plt.plot(A[0], A[1], 'o')
 plt.text(A[0] * (1 + 0.001), A[1] * (1 - 0.01) , 'k1')
 plt.plot(B[0], B[1], 'o')
